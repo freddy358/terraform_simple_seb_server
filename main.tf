c1e31a5d850a921ccc8d760961790b17e835a49a
Farid Bakhishli <farid.bakhishli@gmail.com>
2021-10-03 18:46:25 +0400

    first commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIATXG2KUDTVJQINBFE"
+  secret_key = "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"
+}
+
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-03a71cec707bfc3d7"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+  tags = {
+    Name = "Web Server Build by Terraform"
+    Owner = "Farid Bakhishli"
+  }
+}
+
+
+resource "aws_security_group" "webserver" {
+  name = "WebServer Security Group"
+  description = "Security Group for web server"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web Server"
+    Owner = "Farid Bakhishli"
+  }
+}
