7903cc6533fe8a8697a94bdb3328fc9d180891bc
Farid Bakhishli <farid.bakhishli@gmail.com>
2021-10-15 14:37:41 +0400

    add some outputs

--- web_server.tf
+++ web_server.tf
@@ -59,3 +59,11 @@ output "webserver_instance_id" {
 output "public_ip_address" {
   value = aws_eip.my_static_ip.public_ip
 }
+
+output "security_group_id" {
+  value = aws_security_group.webserver.id
+}
+
+output "security_group_arn" {
+  value = aws_security_group.webserver.arn
+}
