10d58c90f7c5cc59c81437f0e69983ae3d57b756
Farid Bakhishli <farid.bakhishli@gmail.com>
2021-10-15 12:59:15 +0400

    add dynamic blok for sg

--- web_server.tf
+++ web_server.tf
@@ -22,9 +22,9 @@ resource "aws_security_group" "webserver" {
   dynamic "ingress" {
     for_each = ["80", "443"]
     content {
-      from_port = dynamic.value
-      to_port   = dynamic.value
-      protocol  = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
