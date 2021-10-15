d81937cd4f2a9bd1ada193b6c94dd3de8de4a35a
Farid Bakhishli <farid.bakhishli@gmail.com>
2021-10-15 14:23:19 +0400

    add outputs

--- web_server.tf
+++ web_server.tf
@@ -20,6 +20,13 @@ resource "aws_instance" "my_webserver" {
     Owner = "Farid Bakhishli"
   }
 
+output "webserver_instance_id" {
+  value = "aws_instance.my_webserver.id"
+}
+
+output "public_ip_address" {
+  value = "aws_eip.my_static_ip.public_ip"
+}
 
   lifecycle {
     create_before_destroy = true
