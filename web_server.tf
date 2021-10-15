a3ee63ce48c89ab9d7ea08cd9c4a06b0fdc836a8
Farid Bakhishli <farid.bakhishli@gmail.com>
2021-10-15 14:28:32 +0400

    little fixes

--- web_server.tf
+++ web_server.tf
@@ -53,9 +53,9 @@ resource "aws_security_group" "webserver" {
 }
 
 output "webserver_instance_id" {
-  value = "aws_instance.my_webserver.id"
+  value = aws_instance.my_webserver.id
 }
 
 output "public_ip_address" {
-  value = "aws_eip.my_static_ip.public_ip"
+  value = aws_eip.my_static_ip.public_ip
 }
